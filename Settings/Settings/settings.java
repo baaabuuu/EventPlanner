89272e5dc1719fed0d88eabf272941acaac478c7
baaabuuu <aradar@live.dk>
2017-03-27 18:58:52 +0200

    Added additional support for workWeek.

--- Settings/Settings/settings.java
+++ Settings/Settings/settings.java
@@ -7,11 +7,28 @@ import java.util.GregorianCalendar;
 /**
  * @author s164166, s164147, s160902
  * This class is used for constants, we will be using it to store constants such as maximum projects a worker can be on.
- * This will make it easier for us to do Tasks in the future. 
+ * This will make it easier for us to do tasks in the future. 
  */
-public class settings {
-	public final static int		maxAssignments	=	20;
-	public final static String	applicationName =	"Work Planner";	
-	public final Date 			startupDate 	= new GregorianCalendar(2017, Calendar.APRIL, 10).getTime();
+public class settings 
+{
+	public final static int maxAssignments		 	=	20;
+	public final static String applicationName		=	"Work Planner";
+	public final static	Date startupDate 			=	new GregorianCalendar(2017, Calendar.APRIL, 10).getTime();
+	private int weekNumber							=	0;
+	
+	
+	
+	/**
+	 * Added so we can advance weeks, 
+	 */
+	public void updateWeek()
+	{
+		weekNumber++;
+	}
+	
+	public int getWeekNumber()
+	{
+		return weekNumber;
+	}
 }
 
