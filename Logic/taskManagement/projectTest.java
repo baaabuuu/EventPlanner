a14882387a7c8efd7f3f081ecf13b68dd625b381
chennex <chennex@NotABird>
2017-03-27 16:48:38 +0200

    Unittest for project/tasks

--- Logic/taskManagement/projectTest.java
+++ Logic/taskManagement/projectTest.java
@@ -1,19 +1,41 @@
 package taskManagement;
 
+import static org.junit.Assert.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import workerLogic.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class projectTest {
 	
-	@test
+	Project project = new Project(null, "testname");
+	//@Test
+	@Before
 	//Create project with name and two workers
 	public void testCreateProject()
 	{
-		List<Worker> workers = new ArrayList<Worker>();
-		workers.add(new Worker("aegfuhr",2));
-		workers.add(new Worker("aeg",5));
-		Project project = new Project(workers);
-		
+		project.addWorker(new Worker("aegfuhr",2));
+		project.addWorker(new Worker("aeg",5));
+		assertEquals("testname",project.getName());
+	}
+	//@Test
+	public void searchWorker()
+	{
+		List<Worker> workerSearch = project.getWorkerbyID(5);
+		assertEquals(workerSearch.size(), 1);
+	}
+	@Test
+	public void addTasktest()
+	{
+		String taskName = "testTask";
+		int[] completionDate = {2017, 3, 27};
+		Task testTask = new Task(taskName, "This is a test task for testing purposes", null,completionDate);
+		project.addTask(testTask);
+		List<Task> projectTasks = project.getTaskbyName(taskName);
+		assertEquals(1,projectTasks.size());
+		assertEquals("testTask",projectTasks.get(0).getName());
 	}
 }
